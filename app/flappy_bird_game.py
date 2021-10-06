0b1addd49b2c093d4caa6f88fb75b30b455ff853
Robert Powel <48132667+rpowel@users.noreply.github.com>
2021-10-06 08:55:30 -0400

    Created Ground Object

--- app/flappy_bird_game.py
+++ app/flappy_bird_game.py
@@ -126,6 +126,30 @@ class Pipe:
         return False
 
 
+class Ground:
+    VELOCITY = 5
+    WIDTH = GROUND_IMAGE.get_width()
+    IMAGE = GROUND_IMAGE
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.width
+
+    def move(self):
+        self.x1 -= self.VELOCITY
+        self.x2 -= self.VELOCITY
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, window):
+        window.blit(self.IMAGE, (self.x1, self.y))
+        window.blit(self.IMAGE, (self.x2, self.y))
+
+
 def draw_window(window, bird):
     window.blit(BACKGROUND_IMAGE, (0, 0))
     bird.draw(window)
